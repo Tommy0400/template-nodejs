395d8b933f8e1efa50259f27ca0e2763a811b3f0
Tommy0400 <117833637+Tommy0400@users.noreply.github.com>
2023-11-12 05:50:24 +0100

    app.js
    Signed-off-by: Tommy0400 <117833637+Tommy0400@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
+const TelegramBot = require('node-telegram-bot-api');
 
-app.use(express.static('public'))
+// Configura el token de tu bot de Telegram
+const telegramToken = '6631269530:AAEP084xOypzZlYiwD3sG7lkdwpscq4SgbE';
+const bot = new TelegramBot(telegramToken, { polling: true });
+
+// Maneja el comando de inicio (/start)
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, '¡Hola! Soy tu bot de Telegram. ¡Bienvenido!');
+});
+
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     res.redirect('/');
-})
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
